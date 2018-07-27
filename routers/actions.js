de832f6e81274fb2fe90010130a5572d1f022c29
Allen Hai <allenhai03@gmail.com>
2018-07-27 12:36:58 -0500

    add POST handler for /actions

--- routers/actions.js
+++ routers/actions.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const actions = express.Router()
+const Actions = require('../data/helpers/actionModel')
 
-actions.post('/', (req, res, next) => {
+actions.post('/', async (req, res, next) => {
+  checkActionBody(req, next)
+
+  const { 
+    project_id, 
+    description, 
+    notes 
+  } = req.body
+
+  const actionBody = notes 
+    ? { project_id, description, notes }
+    : { project_id, description, notes: "" }
+
+  try {
+    const action = await Actions.insert(actionBody)
+    res.status(200).json(action)
+  } catch(e) {
+    sendError(500, e.message, next)
+  }
 })
 
 actions.get('/', (req, res, next) => {
@@ -22,5 +41,18 @@ actions.use((err, req, res, next) => {
     message: err.message
   })
 })
+
+const checkActionBody = (req, next) => {
+  if (!req.body || !req.body.project_id || !req.body.description){
+    sendError(400, "please provide project_id and description", next)
+  }
+}
+
+const sendError = (code, message, next) => {
+  next({
+    code,
+    message
+  })
+}
 
 module.exports = actions
