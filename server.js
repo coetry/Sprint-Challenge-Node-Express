b924cbc372526aaf5c5d2910a0c5ed4da886bc30
Allen Hai <allenhai03@gmail.com>
2018-07-27 13:12:59 -0500

    add default 404 route middleware for undefined routes

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.json())
 app.use('/projects', projects)
 app.use('/actions', actions)
 
+app.use((req, res) => {
+  res.status(404).send('invalid route')
+})
+
 app.listen(8080, () => {
   console.log('💰💵💸 : 8080')
 })
